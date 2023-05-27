5cd39b3f99152d625265f138b1ff12752fd0d7b2
muhammadusman36 <mu1363131@gmail.com>
2023-05-27 16:21:14 +0500

    updated

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import { connectDB } from "./data/dataBase.js";
 
 connectDB();
 
+app.get("/", (req, res) => {
+  res.send("It's Working fine!");
+});
 app.listen(process.env.PORT, () => {
   console.log(
     `server is running on PORT ==> ${process.env.PORT} in ${process.env.NODE_ENV} Mode`
